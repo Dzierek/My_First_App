9cb2d2e6115ac93d8684194911d63672133bbe42
Dzierek <122981907+Dzierek@users.noreply.github.com>
2026-02-19 17:16:29 +0100

    Delete project directory

--- project/scripts/before_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y python3
-rm -rf /home/ec2-user/app
-mkdir -p /home/ec2-user/app
--- project/scripts/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/app
-pkill -f gunicorn || true
-nohup gunicorn -w 2 -b 0.0.0.0:5000 app:app &
