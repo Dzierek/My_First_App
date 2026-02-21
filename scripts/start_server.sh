0f2b55e6530b421a0eafc655d18a2565ce07896e
Dzierek <122981907+Dzierek@users.noreply.github.com>
2026-02-21 13:13:17 +0100

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo yum update -y
+sudo yum install python3 python3-pip -y
+sudo pip3 install flask
 cd /home/ec2-user/app
 pkill -f gunicorn || true
 nohup gunicorn -w 2 -b 0.0.0.0:5000 app:app &
