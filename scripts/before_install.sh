572c8fbc020c0a7b3810f0882056ada68afad127
Dzierek <122981907+Dzierek@users.noreply.github.com>
2026-02-24 17:00:42 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-yum update -y
-yum install -y python3
-rm -rf /home/ec2-user/app
-mkdir -p /home/ec2-user/app
+set -e  # zatrzymaj skrypt przy pierwszym błędzie
+echo "=== BeforeInstall: Przygotowanie środowiska ==="
+#yum update -y
+#yum install -y python3
+#rm -rf /home/ec2-user/app
+#mkdir -p /home/ec2-user/app
+sudo yum update -y # Aktualizacja pakietów
+sudo yum install -y python3 python3-pip # Instalacja Pythona i pip (jeśli nie istnieją)
+mkdir -p /home/ec2-user/app # Utworzenie katalogu dla aplikacji (jeśli nie istnieje)
+chown -R ec2-user:ec2-user /home/ec2-user/app # Nadanie odpowiednich uprawnień
+echo "=== BeforeInstall zakończony ==="
