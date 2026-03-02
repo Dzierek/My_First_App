0a3bf38fbeb5b118f711fdc4dcb9e2cced640959
unknown <mati.technic@gmail.com>
2026-03-02 22:22:19 +0100

    Wiktor

--- app1/app.py
+++ app1/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello DevOps Cierek - first app!!" \
+    return "Hello DevOps Cierek - first app Wiktor!!" \
 
 @app.route("/write")
 def write_file():
